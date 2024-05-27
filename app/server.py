c8c63e4a66c0ee6927b20a39f7bbf7fd223f9c83
Babapiroo <huseyindurmus313131@gmail.com>
2024-05-27 21:05:11 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,5 +1,5 @@
 import subprocess as sp
 
 if True:
-  sp.run("pip install notebook && jupyter notebook --ip=0.0.0.0 --allow-root --no-browser", stdout=sp.PIPE, shell=True)
+  sp.run("pip install notebook && jupyter notebook --allow-root --no-browser", stdout=sp.PIPE, shell=True)
   print(sp.stdout)
