a19ff57ca81ff1cb175e22d0cb3294cce10eca0a
Babapiroo <huseyindurmus313131@gmail.com>
2024-05-27 21:20:45 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo -e "e\ne" | jupyter notebook password
 apt update -y
 apt install npm -y
 npm i pm2 -g
-pm2 start "jupyter notebook --ip=0.0.0.0 --port=8080 --allow-root"
+pm2 start "jupyter notebook --allow-root"
