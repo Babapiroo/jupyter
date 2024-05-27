0e704871b6fb4bb20dea0a77295c025038c186f5
Babapiroo <huseyindurmus313131@gmail.com>
2024-05-27 21:39:08 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 pip install jupyter
 echo -e "e\ne" | jupyter notebook password 
 apt update -y
-jupyter notebook --host=0.0.0.0 --port=8080  --allow-root
+jupyter notebook --host=127.0.0.1 --port=8080  --allow-root
